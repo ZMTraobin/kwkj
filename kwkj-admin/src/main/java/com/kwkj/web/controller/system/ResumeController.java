1cfe721d56c7b6b258667e71b1d189ef98e51a03
57202 <57202@DESKTOP-DQK8J3O>
2019-09-29 22:32:53 +0800

    #简历反馈#面试管理#offer管理#入职人员管理（更新）-zhanghuan-2019/9/29

--- kwkj-admin/src/main/java/com/kwkj/web/controller/system/ResumeController.java
+++ kwkj-admin/src/main/java/com/kwkj/web/controller/system/ResumeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,7 +39,7 @@ public class ResumeController extends BaseController
 	 */
 	@RequestMapping("/list")
 	@ResponseBody
-	public TableDataInfo list(Resume resume)
+	public TableDataInfo list(@RequestBody Resume resume)
 	{
 		startPage();
         List<Resume> list = resumeService.selectResumeList(resume);
